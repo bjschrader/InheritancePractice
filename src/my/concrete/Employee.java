2d2fe14332c246038f803c63c8abb3881b0d2f54
schra_000 <schra_000@Brian>
2014-08-31 16:22:19 -0500

    A little more progress

--- src/my/concrete/Employee.java
+++ src/my/concrete/Employee.java
@@ -6,4 +6,31 @@ package my.concrete;
  */
 public class Employee {
     
+    
+    private int empNum;
+    private String empName;
+    
+    public Employee(){
+    
+    }
+    public Employee(int empNum, String empName){
+        
+    }
+
+    public int getEmpNum() {
+        return empNum;
+    }
+
+    public void setEmpNum(int empNum) {
+        this.empNum = empNum;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+    
 }
--- src/my/concrete/HourlyEmployee.java
+++ src/my/concrete/HourlyEmployee.java
@@ -5,5 +5,12 @@ package my.concrete;
  * @author schra_000
  */
 public class HourlyEmployee extends Employee {
+    public static void main(String[] args) {
+        
+        //HourlyEmployee hEmp1 = new HourlyEmployee();
+        
     
+
+    
+}
 }
